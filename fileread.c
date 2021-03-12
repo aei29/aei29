e48bf477a3554171a633801dfebee8c14869368b
aei29 <aei29@foxmail.com>
2021-03-12 22:34:29 +0800

    1

---
+++ fileread.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE 1024
+int main()
+{
+ char buf[MAX_LINE];  
+ FILE *fp;            
+ int len;             
+ if((fp = fopen("G:\\sx\\user_login.txt","r")) == NULL)
+ {
+ perror("fail to read");
+ exit (1) ;
+ }
+ while(fgets(buf,MAX_LINE,fp) != NULL)
+ {
+ len = strlen(buf);
+ buf[len-1] = '\0';  
+ printf("%s %d \n",buf,len - 1);
+ }
+  return 0;
+}
